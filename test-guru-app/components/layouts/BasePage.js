c04e7da91750f58d4e20a9790724d135be4871f7
Khanh Tuong <khanhtuongnt@gmail.com>
2020-08-15 18:26:14 +0700

    Ok

---
+++ test-guru-app/components/layouts/BasePage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+const BasePage = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
+        <title>Test Guru</title>
+        <link type="image/x-icon" href="assets/img/favicon.png" rel="icon" />
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
+        <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="assets/css/owl.theme.default.min.css" />
+        <link rel="stylesheet" href="assets/css/style.css" />
+        <script src="assets/js/jquery.min.js" />
+		    <script src="assets/js/popper.min.js" />
+    		<script src="assets/js/bootstrap.min.js" />
+    		<script src="assets/js/owl.carousel.min.js" />
+    		<script src="assets/js/script.js" />
+        <script src="assets/js/html5shiv.min.js" />
+			  <script src="assets/js/respond.min.js" />
+      </Head>
+      <div className="main-wrapper">
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default BasePage;
---
+++ test-guru-app/components/layouts/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-fixed">
+        <nav className="navbar navbar-expand-lg header-nav">
+          <div className="navbar-header">
+            <a id="mobile_btn" href="#" onClick={e => e.preventDefault()}>
+              <span className="bar-icon">
+                <span />
+                <span />
+                <span />
+              </span>
+            </a>
+            <a href="index.html" className="navbar-brand logo">
+              <img src="assets/img/logo.png" className="img-fluid" alt="Logo" />
+            </a>
+          </div>
+          <div className="main-menu-wrapper">
+            <div className="menu-header">
+              <a href="index.html" className="menu-logo">
+                <img src="assets/img/logo.png" className="img-fluid" alt="Logo" />
+              </a>
+              <a id="menu_close" className="menu-close" href="#" onClick={e => e.preventDefault()}>
+                <i className="fas fa-times" />
+              </a>
+            </div>
+            <ul className="main-nav">
+              <li className="active">
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li className="login-link">
+                <Link href="login">
+                  <a>Login / Signup</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <ul className="nav header-navbar-rht">
+            <li className="nav-item">
+              <Link href="login">
+                <a className="nav-link">Login</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="register">
+                <a className="nav-link header-login" >Register</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+  )
+}
+
+export default Header;
