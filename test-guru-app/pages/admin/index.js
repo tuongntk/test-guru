7602d16dbea150a2811b8ba1ea947e88ec51e440
Khanh Tuong <khanhtuongnt@gmail.com>
2020-08-22 10:46:50 +0700

    Ok

--- test-guru-app/pages/admin/index.js
+++ test-guru-app/pages/admin/index.js
@@ -1,8 +1,5 @@
-  
+
 import React, { useCallback, useMemo, useState } from 'react'
-import { Editor, Transforms, createEditor } from 'slate'
-import { Editable, withReact, useSlate, Slate } from 'slate-react'
-import { withHistory } from 'slate-history'
 
 import BasePage from '../../components/layouts/admin/BasePage'
 import Header from '../../components/layouts/admin/Header'
@@ -10,133 +7,42 @@ import PageWrapper from '../../components/layouts/admin/PageWrapper'
 import PageHeader from '../../components/layouts/admin/PageHeader'
 import SideBar from '../../components/layouts/admin/SideBar'
 
-import { Button, Icon, Toolbar } from '../../components/layouts/admin/Editor/ToolBar'
-const LIST_TYPES = ['numbered-list', 'bulleted-list']
+import RichMarkDownEditor from "rich-markdown-editor";
 
-const initialValue = [
-  {
-    children: [
-      { text: 'This is editable plain text, just like a <textarea>!' },
-    ],
-  },
-]
+class YoutubeEmbed extends React.Component {
+  render() {
+    const { attrs } = this.props;
+    const videoId = attrs.matches[1];
 
-const toggleBlock = (editor, format) => {
-  const isActive = isBlockActive(editor, format)
-  const isList = LIST_TYPES.includes(format)
-
-  Transforms.unwrapNodes(editor, {
-    match: n => LIST_TYPES.includes(n.type),
-    split: true,
-  })
-
-  Transforms.setNodes(editor, {
-    type: isActive ? 'paragraph' : isList ? 'list-item' : format,
-  })
-
-  if (!isActive && isList) {
-    const block = { type: format, children: [] }
-    Transforms.wrapNodes(editor, block)
+    return (
+      <p align="center">
+        <iframe
+          src={`https://www.youtube.com/embed/${videoId}?modestbranding=1`}
+          width='640'
+          height='480'
+        />
+      </p>
+    );
   }
-}
-
-const toggleMark = (editor, format) => {
-  const isActive = isMarkActive(editor, format)
-
-  if (isActive) {
-    Editor.removeMark(editor, format)
-  } else {
-    Editor.addMark(editor, format, true)
-  }
-}
-
-const isBlockActive = (editor, format) => {
-  const [match] = Editor.nodes(editor, {
-    match: n => n.type === format,
-  })
-
-  return !!match
-}
-
-const isMarkActive = (editor, format) => {
-  const marks = Editor.marks(editor)
-  return marks ? marks[format] === true : false
-}
-
-const Element = ({ attributes, children, element }) => {
-  switch (element.type) {
-    case 'block-quote':
-      return <blockquote {...attributes}>{children}</blockquote>
-    case 'bulleted-list':
-      return <ul {...attributes}>{children}</ul>
-    case 'heading-one':
-      return <h1 {...attributes}>{children}</h1>
-    case 'heading-two':
-      return <h2 {...attributes}>{children}</h2>
-    case 'list-item':
-      return <li {...attributes}>{children}</li>
-    case 'numbered-list':
-      return <ol {...attributes}>{children}</ol>
-    default:
-      return <p {...attributes}>{children}</p>
-  }
-}
-
-const Leaf = ({ attributes, children, leaf }) => {
-  if (leaf.bold) {
-    children = <strong>{children}</strong>
-  }
-
-  if (leaf.code) {
-    children = <code>{children}</code>
-  }
-
-  if (leaf.italic) {
-    children = <em>{children}</em>
-  }
-
-  if (leaf.underline) {
-    children = <u>{children}</u>
-  }
-
-  return <span {...attributes}>{children}</span>
-}
-
-const BlockButton = ({ format, icon }) => {
-  const editor = useSlate()
-  return (
-    <Button
-      active={isBlockActive(editor, format)}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleBlock(editor, format)
-      }}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  )
-}
-
-const MarkButton = ({ format, icon }) => {
-  const editor = useSlate()
-  return (
-    <Button
-      active={isMarkActive(editor, format)}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleMark(editor, format)
-      }}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  )
 }
 
 export default function Home() {
-  const [value, setValue] = useState(initialValue)
-  const renderElement = useCallback(props => <Element {...props} />, [])
-  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
-  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+  const [setting, setSetting] = useState({
+    readOnly: false,
+    template: false,
+    value: undefined,
+  })
+
+  const handleToggleTemplate = () => {
+    setSetting({ template: !setting.template })
+  };
+
+  const handleUpdateValue = () => {
+    const existing = "";
+    const value = `${existing}\n\nedit!`;
+
+    setSetting({ value })
+  };
 
   return (
     <BasePage>
@@ -195,26 +101,42 @@ export default function Home() {
                           <div className="card-body">
                             <div className="row">
                               <div className="col-12 blog-details">
-                                <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-                                    <Toolbar>
-                                      <MarkButton format="bold" icon="format_bold" />
-                                      <MarkButton format="italic" icon="format_italic" />
-                                      <MarkButton format="underline" icon="format_underlined" />
-                                      <MarkButton format="code" icon="code" />
-                                      <BlockButton format="heading-one" icon="looks_one" />
-                                      <BlockButton format="heading-two" icon="looks_two" />
-                                      <BlockButton format="block-quote" icon="format_quote" />
-                                      <BlockButton format="numbered-list" icon="format_list_numbered" />
-                                      <BlockButton format="bulleted-list" icon="format_list_bulleted" />
-                                    </Toolbar>
-                                  <Editable  renderElement={renderElement}
-                                    renderLeaf={renderLeaf}
-                                    placeholder="Enter some rich text…"
-                                    spellCheck
-                                    autoFocus />
-                                </Slate>
+                                <RichMarkDownEditor
+                                  defaultValue="Hello world!"
+                                  value={setting.value}
+                                  template={setting.template}
+                                  uploadImage={file => {
+                                    console.log("File upload triggered: ", file);
+                                    // Delay to simulate time taken to upload
+                                    return new Promise(resolve => {
+                                      setTimeout(
+                                        () => resolve("https://loremflickr.com/1000/1000"),
+                                        1500
+                                      );
+                                    });
+                                  }}
+                                  embeds={[
+                                    {
+                                      title: "YouTube",
+                                      keywords: "youtube video tube google",
+                                      icon: () => (
+                                        <img
+                                          src="https://upload.wikimedia.org/wikipedia/commons/7/75/YouTube_social_white_squircle_%282017%29.svg"
+                                          width={24}
+                                          height={24}
+                                        />
+                                      ),
+                                      matcher: url => {
+                                        return url.match(
+                                          /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{11})$/i
+                                        );
+                                      },
+                                      component: YoutubeEmbed,
+                                    },
+                                  ]}
+                                />
                               </div>
-                            </div>  
+                            </div>
                           </div>
                         </div>
 
@@ -242,7 +164,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-
 
       </PageWrapper>
     </BasePage>
