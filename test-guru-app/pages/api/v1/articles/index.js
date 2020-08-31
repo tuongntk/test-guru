020a1cced265796e657fd9e18380dc1192ef4929
Khanh Tuong <khanhtuongnt@gmail.com>
2020-08-31 16:45:24 +0700

    ok

--- test-guru-app/pages/api/v1/articles/index.js
+++ test-guru-app/pages/api/v1/articles/index.js
@@ -3,21 +3,39 @@
 import ArticleApi from '@/libs/api/articles'
 // import auth0 from '@/utils/auth0';
 
-// export default async function createPortfolio(req, res) {
-//   try {
-//     const { accessToken } = await auth0.getSession(req);
-//     const json = await new PortfolioApi(accessToken).create(req.body);
-//     return res.json(json.data);
-//   } catch(e) {
-//     return res.status(e.status || 422).json(e.response.data);
-//   }
-// }
+async function createArticle(req, res) {
+  try {
+    // const { accessToken } = await auth0.getSession(req);
+    const accessToken = null;
+    const json = await new ArticleApi(accessToken).create(req.body);
+    return res.json(json.data);
+  } catch(e) {
+    return res.status(e.status || 400).json(e.response.data);
+  }
+}
 
-export default async function getArticles(req, res) {
+async function getArticles(req, res) {
   try {
     const { data } = await new ArticleApi().getAll();
     return res.json(data);
   } catch(e) {
-    return res.status(e.status || 422).json(e.response.data);
+    return res.status(e.status || 400).json(e.response.data);
   }
+}
+
+export default async function handleArticle(req, res) {
+  if (req.method === 'GET') {
+    const json = await getArticles(req, res);
+    return res.json(json.data);
+  }
+
+  // if (req.method === 'POST') {
+  //   try {
+  //     const { accessToken } = await auth0.getSession(req);
+  //     const json = await new BlogApi(accessToken).update(req.query.id, req.body);
+  //     return res.json(json.data);
+  //   } catch(e) {
+  //     return res.status(e.status || 422).json(e.response.data);
+  //   }
+  // }
 }
