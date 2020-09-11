0ee3a4ed17fbdd01979a4011770f430eb1490adc
Khanh Tuong <khanhtuongnt@gmail.com>
2020-09-11 23:27:51 +0700

    ok

--- test-guru-app/pages/api/v1/articles/[id].js
+++ test-guru-app/pages/api/v1/articles/[id].js
@@ -1,24 +1,19 @@
-import dbConnect from '../../../db/dbConnect'
-import Categories from '../../../db/models/Categories'
-
 export default async function handler(req, res) {
   const { query: { id }, method } = req
 
-  await dbConnect()
-
   switch (method) {
-    case 'GET':
-      try {
-        const category = await Categories.findById(id)
-        if (!category) {
-          return res.status(400).json({ success: false })
-        }
+    // case 'GET':
+    //   try {
+    //     const category = await Categories.findById(id)
+    //     if (!category) {
+    //       return res.status(400).json({ success: false })
+    //     }
 
-        res.status(200).json({ success: true, data: category })
-      } catch (error) {
-        res.status(400).json({ success: false })
-      }
-      break
+    //     res.status(200).json({ success: true, data: category })
+    //   } catch (error) {
+    //     res.status(400).json({ success: false })
+    //   }
+    //   break
 
     // case 'PUT':
     //   try {
